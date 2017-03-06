5a23fe9cd4c3fd3d5c2964b921329f3221739f73
Mark Gaulton <mark.gaulton@uwaterloo.ca>
2017-03-06 13:23:49 -0500

    modified:   updatesub.sh

--- updatesub.sh
+++ updatesub.sh
@@ -1,4 +1,4 @@
-for each in $(ls -1t); do sh /fsys2/mgaulton/gits/kalitools/updatesub.sh $each ; done
+#for each in $(ls -1t); do sh /fsys2/mgaulton/gits/kalitools/updatesub.sh $each ; done
 
 cd $1
 git checkout master
