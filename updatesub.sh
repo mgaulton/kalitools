a2d22ada98543be0552edaaf035cea6c9016c4c5
Mark Gaulton <markgaulton@gmail.com>
2018-04-09 14:07:03 -0400

    updates

--- updatesub.sh
+++ updatesub.sh
@@ -1,12 +1,10 @@
 #for each in $(ls -1t); do sh /fsys2/mgaulton/gits/kalitools/updatesub.sh $each ; done
 git config --global alias.pullall '!f(){ git pull "$@" && git submodule update --init --recursive; }; f'
 
-git submodule foreach --recursive git submodule update --init
-
-#d $1
-#it checkout master
-#it submodule update
-#it pull
+git submodule foreach --recursive git submodule update 
+git submodule foreach 'git checkout master'
+git submodule update
+git submodule status --recursive
 
 git add .
 git commit -m 'updates'
